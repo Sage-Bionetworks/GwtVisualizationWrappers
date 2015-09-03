26aab6a47c72d23ca20da2e5a1270126e2e61695
Jay Hodgson <jay.hodgson@sagebase.org>
2015-09-03 12:04:31 -0700

    move source and visualization config interface into gwt visualization library

--- gwtvisualizationwrappers/src/main/java/org/gwtvisualizationwrappers/client/biodalliance/Biodalliance013dev.java
+++ gwtvisualizationwrappers/src/main/java/org/gwtvisualizationwrappers/client/biodalliance/Biodalliance013dev.java
@@ -1,5 +1,8 @@
 package org.gwtvisualizationwrappers.client.biodalliance;
 
+import java.util.Date;
+import java.util.List;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.core.client.JsonUtils;
@@ -63,7 +66,21 @@ public class Biodalliance013dev {
 	}]
 }
 	 */
-	public void show(JavaScriptObject biodallianceBrowserConfig) {
+	/**
+	 * 
+	 * @param containerId div element id to inject Biodalliance visualization
+	 * @param initChr init chr
+	 * @param initViewStart init view start
+	 * @param initViewEnd init view end
+	 * @param currentConfig genome browser configuration
+	 * @param sources source tracks
+	 */
+	public void show(String containerId, 
+			String initChr,
+			int initViewStart,
+			int initViewEnd,
+			BiodallianceConfigInterface currentConfig,
+			List<BiodallianceSource> sources) {
 		//lazy load the cytoscape.js source
 		if (!isBiodalliance013DevLoaded()) {
 		    ScriptInjector.fromString(BiodallianceClientBundle.INSTANCE.biodalliance0_13dev().getText())
@@ -72,12 +89,188 @@ public class Biodalliance013dev {
 		    _init013Dev();
 		}
 
-		_initBrowser(biodallianceBrowserConfig);
+		JavaScriptObject config = createNewBiodallianceBrowserConfig(containerId, initChr, initViewStart, initViewEnd, currentConfig);
+		
+		//add a source(s)
+		for (BiodallianceSource source : sources) {
+			if (BiodallianceSource.SourceType.BIGWIG.equals(source.getSourceType())) {
+				addBigwigSource(config, source);	
+			} else if (BiodallianceSource.SourceType.VCF.equals(source.getSourceType())) {
+				addVCFSource(config, source);
+			}
+		}
+		
+		_initBrowser(config);
 	}
 
 	private static native void _init013Dev() /*-{
 		$wnd.Browser013Dev = $wnd.Browser;
 	}-*/;
 
+	
+	
 
+	private JavaScriptObject createNewBiodallianceBrowserConfig(
+			String containerId,
+			String initChr,
+			int initViewStart,
+			int initViewEnd,
+			BiodallianceConfigInterface config
+			) {
+		
+		return createNewBiodallianceBrowserConfig(containerId, initChr, initViewStart, initViewEnd, config.getTwoBitURI(),
+				config.getBwgURI(), config.getStylesheetURI(), config.getTrixURI(), config.getSpeciesName(), config.getTaxon(), config.getAuthName(),
+				config.getVersion(), config.getUscsName());
+	}
+	
+	private native JavaScriptObject createNewBiodallianceBrowserConfig(
+			String containerId,
+			String initChr,
+			int initViewStart,
+			int initViewEnd,
+			String genomeFileURI,
+			String gencodeBBFileURI,
+			String gencodeXMLFileURI, //stylesheet
+			String gencodeIndexFileURI,
+			String coordSystemSpeciesName,
+			int coordSystemTaxon,
+			String coordSystemAuth,
+			String coordSystemVersion,
+			String coordSystemUcscName
+			) /*-{
+		var resolverFunction = function(url) {
+		   return fetch(url, {  
+			  credentials: 'include'  //sending credentials with a fetch request (session cookie)
+			}).then(function(resp) {
+		       return resp.json();
+		   }).then(function(rdata) {
+		       return rdata.url;
+		   });
+		}
+				
+		var biodallianceBrowserConfig = {
+				pageName: containerId,
+				noPersist: true,
+				chr: initChr, 
+				viewStart:  initViewStart, 
+				viewEnd:  initViewEnd, 
+				cookieKey: coordSystemSpeciesName, 
+				fullScreen: false,
+				coordSystem: { 
+					speciesName: coordSystemSpeciesName, 
+					taxon: coordSystemTaxon, 
+					auth: coordSystemAuth, 
+					version: coordSystemVersion, 
+					ucscName: coordSystemUcscName},
+				baseColors: {
+	                 'A': 'black',
+	                 'C': 'black',
+	                 'G': 'black',
+	                 'T': 'black',
+	                 '-': 'black', //deletion
+	                 'I': 'red'    //insertion
+	            },
+				sources: [{name: 'Genome',
+					twoBitURI: genomeFileURI,
+					tier_type: 'sequence',
+					provides_entrypoints: true,
+					pinned: true,
+					resolver: resolverFunction}, 
+
+					{name: 'GENCODE',
+					bwgURI: gencodeBBFileURI,
+					stylesheet_uri: gencodeXMLFileURI,
+					collapseSuperGroups: true, 
+					trixURI: gencodeIndexFileURI,
+					subtierMax:5,
+					pinned:true,
+					resolver: resolverFunction
+					}]
+		};
+		return biodallianceBrowserConfig;
+	}-*/;
+	
+	private void addBigwigSource(
+			JavaScriptObject biodallianceBrowserConfig,
+			BiodallianceSource source) {
+		addBigwigSource(biodallianceBrowserConfig, source.getSourceName(), source.getSourceURI(), source.getStyleType(), source.getStyleGlyphType(), source.getStyleColor(), source.getTrackHeightPx());
+	}
+	private native void addBigwigSource(
+			JavaScriptObject biodallianceBrowserConfig,
+			String sourceName,
+			String sourceBwgURI,
+			String styleType, 
+			String styleGlyphType,
+			String styleColor,
+			int trackHeightPx
+			) /*-{
+		var resolverFunction = function(url) {
+		   return fetch(url, {  
+			  credentials: 'include'  //sending credentials with a fetch request (session cookie)
+			}).then(function(resp) {
+		       return resp.json();
+		   }).then(function(rdata) {
+		       return rdata.url;
+		   });
+		}
+	    var newSource = {
+	    	name: sourceName,
+			collapseSuperGroups:true,
+			bwgURI: sourceBwgURI,
+			style: [{type : styleType,
+					style: {glyph: styleGlyphType,
+							COLOR1:styleColor,
+							COLOR2:styleColor,
+							COLOR3:styleColor,
+							HEIGHT:trackHeightPx}}],
+			collapseSuperGroups: true, 
+			resolver: resolverFunction
+	    }
+	    biodallianceBrowserConfig.sources.push(newSource);
+	}-*/;
+	
+	private void addVCFSource(
+			JavaScriptObject biodallianceBrowserConfig,
+			BiodallianceSource source) {
+		addVCFSource(biodallianceBrowserConfig, source.getSourceName(), source.getSourceURI(), source.getSourceIndexURI(), source.getStyleType(), source.getStyleGlyphType(), source.getStyleColor(), source.getTrackHeightPx());
+	}
+	
+	private native void addVCFSource(
+			JavaScriptObject biodallianceBrowserConfig,
+			String sourceName,
+			String sourceURI,
+			String sourceIndexURI,
+			String styleType, 
+			String styleGlyphType,
+			String styleColor,
+			int trackHeightPx
+			) /*-{
+		var resolverFunction = function(url) {
+		   return fetch(url, {  
+			  credentials: 'include'  //sending credentials with a fetch request (session cookie)
+			}).then(function(resp) {
+		       return resp.json();
+		   }).then(function(rdata) {
+		       return rdata.url;
+		   });
+		}
+	    var newSource = {
+	    	name: sourceName,
+			collapseSuperGroups:true,
+			uri: sourceURI,
+			indexURI: sourceIndexURI,
+			payload: 'vcf',
+			tier_type: 'tabix',
+			style: [{type : styleType,
+					style: {glyph: styleGlyphType,
+							COLOR1:styleColor,
+							COLOR2:styleColor,
+							COLOR3:styleColor,
+							HEIGHT:trackHeightPx}}],
+			collapseSuperGroups: true, 
+			resolver: resolverFunction
+	    }
+	    biodallianceBrowserConfig.sources.push(newSource);
+	}-*/;
+	
 }
---
+++ gwtvisualizationwrappers/src/main/java/org/gwtvisualizationwrappers/client/biodalliance/BiodallianceSource.java
@@ -0,0 +1,210 @@
+package org.gwtvisualizationwrappers.client.biodalliance;
+
+/*
+ * #%L
+ * GwtCytoscapeJs
+ * %%
+ * Copyright (C) 2015 GwtCytoscapeJs
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONParser;
+import com.google.gwt.json.client.JSONString;
+
+/**
+ * Source object represents a Biodalliance track source. 
+ */
+public class BiodallianceSource {
+	String sourceName, sourceURI, entityId, indexEntityId, indexSourceURI;
+	Long version, indexVersion;
+	public static final String DEFAULT_STYLE_TYPE = "default";
+	public static final String DEFAULT_STYLE_GLYPH_TYPE = "HISTOGRAM";
+	public static final String DEFAULT_STYLE_COLOR = "#808080"; //grey
+	public static final Integer DEFAULT_HEIGHT = 30;
+	
+	String styleType, styleGlyphType, styleColor;
+	int trackHeightPx;
+
+	public enum SourceType {
+		BIGWIG, VCF
+	}
+	SourceType sourceType;
+	
+	/**
+	 * json keys
+	 */
+	public static final String SOURCE_NAME_KEY = "name";
+	public static final String SOURCE_TYPE = "type";
+	public static final String SOURCE_ENTITY_ID_KEY = "entityId";
+	public static final String SOURCE_ENTITY_VERSION_KEY = "entityVersion";
+	public static final String SOURCE_INDEX_ENTITY_ID_KEY = "indexEntityId";
+	public static final String SOURCE_INDEX_ENTITY_VERSION_KEY = "indexEntityVersion";
+	public static final String STYLE_TYPE_KEY = "styleType";
+	public static final String STYLE_GLYPH_TYPE_KEY = "styleGlyphType";
+	public static final String STYLE_COLOR_KEY = "color";
+	public static final String STYLE_HEIGHT = "height";
+	
+	public BiodallianceSource() {
+		initDefaults();
+	}
+	
+	public BiodallianceSource(String json) {
+		initializeFromJson(json);
+	}
+	
+	
+	public void initDefaults() {
+		styleType = DEFAULT_STYLE_TYPE;
+		styleGlyphType = DEFAULT_STYLE_GLYPH_TYPE;
+		styleColor = DEFAULT_STYLE_COLOR;
+		trackHeightPx = DEFAULT_HEIGHT;
+	}
+	
+	public JSONObject toJsonObject() {
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put(SOURCE_NAME_KEY, new JSONString(sourceName));
+		jsonObject.put(SOURCE_ENTITY_ID_KEY, new JSONString(entityId));
+		if (version != null) {
+			jsonObject.put(SOURCE_ENTITY_VERSION_KEY, new JSONString(version.toString()));	
+		}
+		if (indexEntityId != null) {
+			jsonObject.put(SOURCE_INDEX_ENTITY_ID_KEY, new JSONString(indexEntityId));	
+		}
+		if (indexVersion != null) {
+			jsonObject.put(SOURCE_INDEX_ENTITY_VERSION_KEY, new JSONString(indexVersion.toString()));	
+		}
+		jsonObject.put(STYLE_TYPE_KEY, new JSONString(styleType));
+		jsonObject.put(STYLE_GLYPH_TYPE_KEY, new JSONString(styleGlyphType));
+		jsonObject.put(STYLE_COLOR_KEY, new JSONString(styleColor));
+		jsonObject.put(SOURCE_TYPE, new JSONString(sourceType.name()));
+		jsonObject.put(STYLE_HEIGHT, new JSONString(Integer.toString(trackHeightPx)));
+		return jsonObject;
+	}
+	
+	public void initializeFromJson(String json) {
+		JSONObject value = (JSONObject)JSONParser.parseStrict(json);
+		setSourceName(value.get(SOURCE_NAME_KEY).isString().stringValue());
+		String entityId = value.get(SOURCE_ENTITY_ID_KEY).isString().stringValue();
+		Long version = null;
+		if (value.containsKey(SOURCE_ENTITY_VERSION_KEY)) {
+			String versionString = value.get(SOURCE_ENTITY_VERSION_KEY).isString().stringValue();
+			version = Long.parseLong(versionString);
+		}
+		setEntity(entityId, version);
+		
+		String indexEntityId = null;
+		if (value.containsKey(SOURCE_INDEX_ENTITY_ID_KEY)) {
+			indexEntityId = value.get(SOURCE_INDEX_ENTITY_ID_KEY).isString().stringValue();
+		}
+		Long indexVersion = null;
+		if (value.containsKey(SOURCE_INDEX_ENTITY_VERSION_KEY)) {
+			String versionString = value.get(SOURCE_INDEX_ENTITY_VERSION_KEY).isString().stringValue();
+			indexVersion = Long.parseLong(versionString);
+		}
+		
+		setIndexEntity(indexEntityId, indexVersion);
+		
+		setStyleType(value.get(STYLE_TYPE_KEY).isString().stringValue());
+		setStyleGlyphType(value.get(STYLE_GLYPH_TYPE_KEY).isString().stringValue());
+		setStyleColor(value.get(STYLE_COLOR_KEY).isString().stringValue());
+		setTrackHeightPx(Integer.parseInt(value.get(STYLE_HEIGHT).isString().stringValue()));
+		String sourceTypeString = value.get(SOURCE_TYPE).isString().stringValue();
+		setSourceType(SourceType.valueOf(sourceTypeString));
+	}
+	
+	public void setStyleType(String styleType) {
+		this.styleType = styleType;
+	}
+
+	public void setStyleGlyphType(String styleGlyphType) {
+		this.styleGlyphType = styleGlyphType;
+	}
+
+	public void setStyleColor(String styleColor) {
+		this.styleColor = styleColor;
+	}
+	
+	public String getSourceName() {
+		return sourceName;
+	}
+	public String getSourceURI() {
+		return sourceURI;
+	}
+	public String getSourceIndexURI() {
+		return indexSourceURI;
+	}
+	public String getStyleType() {
+		return styleType;
+	}
+	public String getStyleGlyphType() {
+		return styleGlyphType;
+	}
+	public String getStyleColor() {
+		return styleColor;
+	}
+	public int getTrackHeightPx() {
+		return trackHeightPx;
+	}
+	
+	public SourceType getSourceType() {
+		return sourceType;
+	}
+	
+	public String getEntityId() {
+		return entityId;
+	}
+
+	public void setEntity(String entityId, Long version) {
+		this.entityId = entityId;
+		this.version = version;
+	}
+
+	public String getIndexEntityId() {
+		return indexEntityId;
+	}
+
+	public void setIndexEntity(String indexEntityId, Long indexVersion) {
+		this.indexEntityId = indexEntityId;
+		this.indexVersion = indexVersion;
+	}
+	
+	public Long getVersion() {
+		return version;
+	}
+	
+	public Long getIndexVersion() {
+		return indexVersion;
+	}
+
+	public void setSourceName(String sourceName) {
+		this.sourceName = sourceName;
+	}
+
+	public void setTrackHeightPx(int trackHeightPx) {
+		this.trackHeightPx = trackHeightPx;
+	}
+	
+	public void setSourceType(SourceType sourceType) {
+		this.sourceType = sourceType;
+	}
+	
+	public void setSourceURI(String sourceURI) {
+		this.sourceURI = sourceURI;
+	}
+	
+	public void setIndexSourceURI(String indexSourceURI) {
+		this.indexSourceURI = indexSourceURI;
+	}
+}
